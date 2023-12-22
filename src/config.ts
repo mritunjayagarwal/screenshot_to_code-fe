5e2dd398579424f929ffdc2c736da84a7f863c99
Mritunjay Agarwal <mritunjayagarwal96@gmail.com>
2023-12-22 06:06:47 +0000

    Added

--- src/config.ts
+++ src/config.ts
@@ -2,11 +2,9 @@
 export const IS_RUNNING_ON_CLOUD =
   import.meta.env.VITE_IS_DEPLOYED === "true" || false;
 
-export const WS_BACKEND_URL =
-  import.meta.env.VITE_WS_BACKEND_URL || "ws://127.0.0.1:7001";
+export const WS_BACKEND_URL = "wss://screenshotbe.onrender.com:443";
 
-export const HTTP_BACKEND_URL =
-  import.meta.env.VITE_HTTP_BACKEND_URL || "http://127.0.0.1:7001";
+export const HTTP_BACKEND_URL = "https://screenshotbe.onrender.com:443";
 
 export const PICO_BACKEND_FORM_SECRET =
   import.meta.env.VITE_PICO_BACKEND_FORM_SECRET || null;
